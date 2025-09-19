ec9f8325ac1e0856d908d4ec6c3bbbad96e6ed10
gokifujiya <goki.fujiya@gmail.com>
2025-09-19 12:18:55 +0900

    Add DAO layer (ComputerPartDAOImpl), models, wrapper, routes, view

--- Routingroutes.php
+++ Routingroutes.php
@@ -1,30 +1,27 @@
 <?php
-use Helpers\DatabaseHelper;
 use Helpers\ValidationHelper;
 use Response\HTTPRenderer;
 use Response\Render\HTMLRenderer;
-use Response\Render\JSONRenderer;
+use Database\DataAccess\Implementations\ComputerPartDAOImpl;
 
 return [
     'random/part'=>function(): HTTPRenderer{
-        $part = DatabaseHelper::getRandomComputerPart();
+        $partDao = new ComputerPartDAOImpl();
+        $part = $partDao->getRandom();
+
+        if($part === null) throw new Exception('No parts are available!');
 
         return new HTMLRenderer('component/computer-part-card', ['part'=>$part]);
     },
     'parts'=>function(): HTTPRenderer{
-        // validation for id
+        // IDの検証
         $id = ValidationHelper::integer($_GET['id']??null);
 
-        $part = DatabaseHelper::getComputerPartById($id);
+        $partDao = new ComputerPartDAOImpl();
+        $part = $partDao->getById($id);
+
+        if($part === null) throw new Exception('Specified part was not found!');
+
         return new HTMLRenderer('component/computer-part-card', ['part'=>$part]);
     }
-    'api/random/part'=>function(): HTTPRenderer{
-        $part = DatabaseHelper::getRandomComputerPart();
-        return new JSONRenderer(['part'=>$part]);
-    },
-    'api/parts'=>function(){
-        $id = ValidationHelper::integer($_GET['id']??null);
-        $part = DatabaseHelper::getComputerPartById($id);
-        return new JSONRenderer(['part'=>$part]);
-    },
 ];
